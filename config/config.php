fc1f63863c821483f1368830a6e32346f6d98e7c
NGUYỄN KHÁNH HƯNG <nguyenkhanhhung20012001@gmail.com>
2022-07-10 23:08:49 +0700

    fix

--- config/config.php
+++ config/config.php
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-<?php
+
+<php
 
 $conn = mysqli_connect('localhost','root','','shop_db') or die('connection failed');
-
-=======
-<?php
-
-$conn = mysqli_connect('localhost','root','','shop_db',3308) or die('connection failed');
-
->>>>>>> origin/phannguyen
 ?>
