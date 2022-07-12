97bf1316767617542be1172bab4c90f571dcf024
linh <linhttbn2001@gmail.com>
2022-07-12 23:49:02 +0700

    linh commit

--- services/CartServices.php
+++ services/CartServices.php
@@ -8,17 +8,17 @@ class CartServices extends MySqlConnect{
      * @param Order $order
      */
 	public function insert($order){
-		$query  = "INSERT INTO `orders`(user_id, name, number, email, method, address, total_products, total_price, placed_on) VALUES("
-		.$order->getUserId().", "
-		.$order->getName().", "
-		.$order->getNumber().", "
-		.$order->getEmail().", "
-		.$order->getMethod().", "
-		.$order->getAddress().", "
-		.$order->getTotalProduct().", "
-		.$order->getTotalPrice().", "
-		.$order->getPlacedOn().")";
-
+		$query  = "INSERT INTO `orders`(user_id, name, number, email, method, address, total_products, total_price, placed_on) VALUES('"
+		.$order->getUserId()."', '"
+		.$order->getName()."', '"
+		.$order->getNumber()."', '"
+		.$order->getEmail()."', '"
+		.$order->getMethod()."', '"
+		.$order->getAddress()."', '"
+		.$order->getTotalProduct()."', '"
+		.$order->getTotalPrice()."', '"
+		.$order->getPlacedOn()."')";
+		echo $query;
 		parent::addQuerry($query);
 		parent::updateQuery($query);
 	}
@@ -42,20 +42,32 @@ class CartServices extends MySqlConnect{
 		parent::addQuerry($query);
 		parent::updateQuery($query);
 	}
+	public function addCart($user_id,$product_name,$product_price,$product_quantity,$product_image){
+		$query = "INSERT INTO `cart`(user_id, name, price, quantity, image) VALUES('$user_id', '$product_name', '$product_price', '$product_quantity', '$product_image')";
+		parent::addQuerry($query);
+		parent::updateQuery($query);
+	}
+	public function getByIdAndName($product_name, $user_id){
+		$query = "SELECT * FROM cart WHERE name = '$product_name' AND user_id = '$user_id'";
+		parent::addQuerry($query);
+        $result = parent::executeQuery();
+        return $result;	
+	}
 
     public function delete($id) {
-        $query = "DELETE FROM `cart` WHERE id = '$id'";
+        $query = "delete FROM `cart` WHERE id = $id";
 
         parent::addQuerry($query);
 		parent::updateQuery($query);
     }
 
     public function deleteAll($user_id) {
-        $query = "DELETE FROM `cart` WHERE user_id = '$user_id'";
+        $query = "delete FROM `cart` WHERE user_id = $user_id";
 
         parent::addQuerry($query);
 		parent::updateQuery($query);
     }
+	
 	public function insertProductToCart($user_id, $product_name,$product_price,$product_quantity, $product_image){
 		$query = "INSERT INTO `cart`(user_id, name, price, quantity, image) VALUES('$user_id', '$product_name', '$product_price', '$product_quantity', '$product_image')";
 		parent::addQuerry($query);
--- services/OrderServices.php
+++ services/OrderServices.php
@@ -3,8 +3,20 @@
 require_once 'MySqlConnect.php';
 class OrderServices extends MySqlConnect{
 
-	public function insert(){
-
+	public function insert($order){
+		$user_id = $order->getUserId();
+		$name = $order->getName();
+		$number = $order->getNumber();
+		$email = $order->getEmail();
+		$method = $order->getMethod();
+		$address = $order->getAddress();
+		$total_products = $order->getTotalProduct();
+		$total_price = $order->getTotalPrice();
+		$placed_on = $order->getPlacedOn();
+		$query="insert INTO `orders`(user_id, name, number, email, method, address, total_products, total_price, placed_on) VALUES('$user_id', '$name', '$number', '$email', '$method', '$address', '$total_products', '$total_price', '$placed_on')";
+		echo $query;
+		parent::addQuerry($query);
+		parent::updateQuery();
 	}
 	public function getAll(){
 		$listOrder = array();
@@ -38,7 +50,7 @@ class OrderServices extends MySqlConnect{
         $result = parent::executeQuery();
 	}
 	public function getFromID($id) {
-		$query = "select * from `orders` where id = '$id'";
+		$query = "select * from `orders` where user_id = $id";
 		parent::addQuerry($query);
         $result = parent::executeQuery();
         return $result;
